ded7048adf4678b8a7d787066107620e73c0032c
Samanvitha0409 <etikyalasamanvitha123@gmail.com>
2026-03-13 09:27:26 +0530

    commited

--- config/db.js
+++ config/db.js
@@ -1,9 +1,25 @@
+// import mongoose from "mongoose";
+// const dbConnect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// export default dbConnect;
 import mongoose from "mongoose";
+
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected ✅");
+    console.log("Host:", conn.connection.host);
+    console.log("Database:", conn.connection.name);
+
   } catch (err) {
-    console.log(err);
+    console.log("DB Error:", err);
   }
 };
+
 export default dbConnect;
